0f04491d02f9d4f7de148678c1cbf95ab4b6141a
ICe <botzer73@gmail.com>
2022-12-08 13:59:29 +0700

    go

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -3,12 +3,17 @@
     <div class="container">
       <div id="header">
         <div id="headTitle">
-          <div id="logo"><img alt="Healthbook" height="44" src="img/logo.png" width="70" /></div>
-          <h1 id="title">メニュー</h1>
+          <div id="logo">
+            <img alt="Healthbook" height="44" src="img/logo.png" width="70" />
+          </div>
+          <h1 id="title">Healthbook</h1>
         </div>
         <ol id="topicPath">
-          <li id="tpHome"><a href="/" title="メニューへ移動する"><img alt="" height="28" src="img/topicPath_home.png"
-                width="28" /></a></li>
+          <li id="tpHome">
+            <a href="/" title="メニューへ移動する"
+              ><img alt="" height="28" src="img/topicPath_home.png" width="28"
+            /></a>
+          </li>
         </ol>
         <div id="headInfo">
           <div id="date-info">
@@ -17,8 +22,11 @@
           </div>
           <div id="user-info">
             <span class="icon"></span>
-            <p id="userName">システム
-              <a href="/login/logout" id="btnlogout" title="ログアウト">ログアウト</a>
+            <p id="userName">
+              システム
+              <a href="/login/logout" id="btnlogout" title="ログアウト"
+                >ログアウト</a
+              >
             </p>
           </div>
         </div>
@@ -31,7 +39,12 @@
             <dl>
               <dt>
                 <router-link to="/viewproduct">
-                  <img alt="タックシール印刷" height="168" src="img/icon-product.png" width="168">
+                  <img
+                    alt="タックシール印刷"
+                    height="168"
+                    src="img/icon-product.png"
+                    width="168"
+                  />
                 </router-link>
               </dt>
               <dd>
@@ -43,7 +56,12 @@
             <dl>
               <dt>
                 <router-link to="/menuclinic">
-                  <img alt="タックシール印刷" height="168" src="img/icon-category.png" width="168">
+                  <img
+                    alt="タックシール印刷"
+                    height="168"
+                    src="img/icon-category.png"
+                    width="168"
+                  />
                 </router-link>
               </dt>
               <dd>
@@ -55,7 +73,12 @@
             <dl>
               <dt>
                 <router-link to="/menumedical">
-                  <img alt="タックシール印刷" height="168" src="img/icon-brand.png" width="168">
+                  <img
+                    alt="タックシール印刷"
+                    height="168"
+                    src="img/icon-brand.png"
+                    width="168"
+                  />
                 </router-link>
               </dt>
               <dd>
@@ -66,8 +89,6 @@
         </ul>
       </div>
     </div>
-
   </div>
-
 </template>
 
