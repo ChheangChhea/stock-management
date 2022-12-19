9e0b506f8cf08692742250dcdcd88ce6c391503d
sonin <sreyninn@gmail.com>
2022-12-19 13:09:14 +0700

    popup

--- resources/js/components/Category/Category.vue
+++ resources/js/components/Category/Category.vue
@@ -190,7 +190,7 @@
             aria-labelledby="staticBackdropLabel"
             aria-hidden="true"
           >
-            <div class="modal-dialog">
+            <div class="modal-dialog smallpopup">
               <div class="modal-content">
                 <div class="modal-header">
                   <h5 class="modal-title" id="staticBackdropLabel">Category</h5>
--- resources/js/components/Supplies/Supplies.vue
+++ resources/js/components/Supplies/Supplies.vue
@@ -79,7 +79,7 @@
                 aria-labelledby="staticBackdropLabel"
                 aria-hidden="true"
               >
-                <div class="modal-dialog">
+                <div class="modal-dialog smallpopup">
                   <div class="modal-content">
                     <div class="modal-header">
                       <h5 class="modal-title" id="staticBackdropLabel">
